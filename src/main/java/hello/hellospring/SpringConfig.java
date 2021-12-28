0d67356903af5a5b6cbd278391e4bd37b12d859b
leesh125 <chelsea12345@naver.com>
2021-12-28 19:49:40 +0900

    스프링 데이터 JPA - JpaRepository interface를 상속받으면 인터페이스명 만으로도 sql문이 알아서 동작 ㄷㄷㄷㄷ

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,29 +12,37 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
-    private EntityManager em;
+//    private DataSource dataSource;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberService memberSevice(){
-        return new MemberService(memberRepository());
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+//    @Autowired
+//    public void setDataSource(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+
+
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        // return new MemoryMemberRepository();
+//        // return new JdbcMemberRepository(dataSource);
+//        // return new JdbcTemplateMemberRepository(dataSource);
+//        // return new JpaMemberRepository(em);
+//    }
 }
